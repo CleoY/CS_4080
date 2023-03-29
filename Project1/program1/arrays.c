761bd6f118d59b1cdd61a6592287e580b38ce2a6
CleoY <77142560+CleoY@users.noreply.github.com>
2023-03-29 13:59:18 -0700

    I think I discovered the problem with the printMatrix() function and it is likely tied to the way 2d array values are stored linearly in memory.

--- Project1/program1/arrays.c
+++ Project1/program1/arrays.c
@@ -12,7 +12,8 @@ bool sizeValidation(int option);
 int randomMatrices(int size);
 //int printMatrix(int rows, int cols, float given[rows][cols], char msg[]);
 //int printMatrix(int cols, float given[][cols]);
-int printMatrix(float** given, int rows, int cols, char msg[]);
+//int printMatrix(float** given, int rows, int cols, char msg[]);
+int printMatrix(int rows, int cols, float given[rows][cols]);
 
 float matrixA[100][100];
 float matrixB[100][100];
@@ -87,6 +88,20 @@ int initializeMatrices(){
         }
     } while(fp == NULL);
 
+
+    // init matrix A
+    for(int i=0; i<100; i++){
+        for(int j=0; j<100; j++){
+            matrixA[i][j] = 1.0;
+        }
+    }
+
+
+
+
+
+
+
     // Read size of matrixA from first line of file
     fscanf(fp, "%d %d", &matrixARows, &matrixACols);
     fscanf(fp, "%*c"); // Clear file reader buffer
@@ -101,10 +116,10 @@ int initializeMatrices(){
     while(rowCounter < matrixARows){
         while(colCounter < matrixACols){        // check != "\n"
             fscanf(fp, "%f", &temp);
-            printf("Temp: %f ", temp);
-            printf("r: %d, c: %d\n", rowCounter, colCounter);
+            //printf("Temp: %f ", temp);
+            //printf("r: %d, c: %d\n", rowCounter, colCounter);
             matrixA[rowCounter][colCounter] = temp;
-            printf(" MatrixA[r][c] = %f\n", matrixA[rowCounter][colCounter]);
+            //printf(" MatrixA[r][c] = %f\n", matrixA[rowCounter][colCounter]);
             colCounter++;
             check = fgetc(fp);
             //printf("check: %d\n", check); //print ascii code of whitespace
@@ -115,26 +130,68 @@ int initializeMatrices(){
         }
         colCounter = 0;
         rowCounter++;
-        printf("row counter: %d\n", rowCounter);
+        //printf("row counter: %d\n", rowCounter);
     }
 
-    printMatrix((float**)matrixA, matrixARows, matrixACols, "MatrixA: ");
+    //printMatrix((float**)matrixA, matrixARows, matrixACols, "MatrixA: ");
 
     // print matrixA
-    //printMatrix(matrixARows, matrixACols, matrixA, "Matrix A:\n");
-    // printf("MatrixA: \n");
-    // // printMatrix(matrixACols, matrixA);
-    // for(int i=0; i<matrixARows; i++){
-    //     for(int j=0; j<matrixACols; j++){
-    //         printf("%f ", matrixA[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-    // printf("\n");
+    // printMatrix(matrixARows, matrixACols, matrixA, "Matrix A:\n");
+    // printMatrix(matrixACols, matrixA);
+    
+    printf("MatrixA: \n");
+    for(int i=0; i<matrixARows; i++){
+        for(int j=0; j<matrixACols; j++){
+            printf("%f ", matrixA[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+    printMatrix(matrixARows, matrixACols, matrixA);
 
     // now upload matrix B
+    fscanf(fp, "%d %d", &matrixBRows, &matrixBCols);
+    fscanf(fp, "%*c"); // Clear file reader buffer
+    printf("MatB size: %d %d\n", matrixBRows, matrixBCols);
 
+    rowCounter = 0;
+    colCounter = 0;
 
+    while(rowCounter < matrixBRows){
+        while(colCounter < matrixBCols){        // check != "\n"
+            fscanf(fp, "%f", &temp);
+            //printf("Temp: %f ", temp);
+            //printf("r: %d, c: %d\n", rowCounter, colCounter);
+            matrixB[rowCounter][colCounter] = temp;
+            //printf(" MatrixB[r][c] = %f\n", matrixB[rowCounter][colCounter]);
+            colCounter++;
+            check = fgetc(fp);
+            //printf("check: %d\n", check); //print ascii code of whitespace
+            if(check == '\n'){
+                //printf("Check is a newline char \n");
+                break;
+            }
+        }
+        colCounter = 0;
+        rowCounter++;
+        //printf("row counter: %d\n", rowCounter);
+    }
+
+    printf("MatrixB: \n");
+    for(int i=0; i<matrixBRows; i++){
+        for(int j=0; j<matrixBCols; j++){
+            printf("%f ", matrixB[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    printMatrix(matrixBRows, matrixBCols, matrixB);
+
+    float matrix[3][4] = {{1.0, 2.0, 3.0, 4.0},
+                          {5.0, 6.0, 7.0, 8.0},
+                          {9.0, 10.0, 11.0, 12.0}};
+    printMatrix(3, 4, matrix);
 
 
     fclose(fp);
@@ -142,6 +199,14 @@ int initializeMatrices(){
 }
 
 float additionOrSubtraction(int option){
+    printf("MatrixA in add: \n");
+    for(int i=0; i<matrixARows; i++){
+        for(int j=0; j<matrixACols; j++){
+            printf("%f ", matrixA[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
     return 0;
 }
 
@@ -181,10 +246,17 @@ int randomMatrices(int size){
     return 0;
 }
 
-int printMatrix(float** given, int rows, int cols, char msg[]){
-
-    printf("%s\n", msg);
-    for(int i = 0; i < rows; i++) {
+// current printMatrix function does not print as expected due to the way that arrays are stored in memory.
+// for the following matrix:
+/**
+ * 1 2 3 0 0 0
+ * 4 5 6 0 0 0 
+ * 7 8 9 0 0 0
+ * it will be stored as 1 2 3 0 0 0 4 5 6 0 0 0 7 8 9 0 0 0
+ * which means you need to increment the row and col counters more to access the next row
+*/
+int printMatrix(int rows, int cols, float given[rows][cols]) {
+    for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             printf("%f ", given[i][j]);
         }
@@ -192,6 +264,18 @@ int printMatrix(float** given, int rows, int cols, char msg[]){
     }
     return 0;
 }
+
+// int printMatrix(float** given, int rows, int cols, char msg[]){
+
+//     printf("%s\n", msg);
+//     for(int i = 0; i < rows; i++) {
+//         for (int j = 0; j < cols; j++) {
+//             printf("%f ", given[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
 
 
 // int printMatrix(int cols, float given[][cols]){
