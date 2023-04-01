7c24b6e99f799d1b0d920771951c6e5644cdaee3
CleoY <77142560+CleoY@users.noreply.github.com>
2023-03-31 17:10:00 -0700

    Just more constructors for program 3.

--- Project1/program3/matrix.cpp
+++ Project1/program3/matrix.cpp
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
 class Matrix{
-    private:
-        float** matrix1;
-        int rows;
-        int cols;
-    
     public:
         Matrix(){
             rows = 0;
@@ -21,15 +16,39 @@ class Matrix{
                 matrix1[i] = new float[cols];
             }
         }
-
-        Matrix(int nRows, int nCols, FILE *fp) : Matrix(nRows, nCols, {}) {
-            
+        Matrix(int nRows, int nCols, FILE *fp){
+            Matrix(nRows, nCols); // I hope this works
 
         }
+
+        
 
         Matrix operator+(const Matrix& matrix2){
             Matrix result(this->rows, this->cols);
             
         }
+
+        Matrix operator-(const Matrix& matrix2){
+
+        }
+
+        int print(){
+            if(matrix1 == NULL){
+                printf("Matrix has not been defined.\n");
+                return 1;
+            }
+            for(int i=0; i<rows; i++){
+                for(int j=0; j<cols; j++){
+                    printf("%f ", matrix1[i][j]);
+                }
+                printf("\n");
+            }
+            return 0;
+        }
+
+    private:
+        float** matrix1;
+        int rows;
+        int cols;
 
 };
